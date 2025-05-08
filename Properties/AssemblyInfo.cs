614ab743030c79bfd04ff3afc43c0e7223d33f9b
Jc Dock <jcdock03@gmail.com>
2025-05-08 16:28:15 +1200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using TotalDailyProfit;
+
+
+[assembly: MelonInfo(typeof(TotalDailyProfit.TotalDailyProfit), "TotalDailyProfit", "1.0.0", "Jc Dock")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TotalDailyProfit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TotalDailyProfit")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63a40101-5f59-4ad6-80a1-99068522f033")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TotalDailyProfit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2CppScheduleOne.UI;
+using Il2CppScheduleOne.Persistence.Datas;
+
+
+
+namespace TotalDailyProfit
+{
+
+
+    public class TotalDailyProfit : MelonMod
+    {
+        public HarmonyLib.Harmony harmony;
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("TotalDailyProfit Initialized");
+            harmony = new HarmonyLib.Harmony("com.jcdock.totaldailyprofit");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Il2CppScheduleOne.UI.DailySummary), "DailySummary")]
+        public class DailySummaryPatch
+        {
+            [HarmonyPatch("Open")]
+            [HarmonyPostfix]
+            public static void Postfix(Il2CppScheduleOne.UI.DailySummary __instance)
+            {
+                var dailySummary = __instance;
+                float playerMoney = dailySummary.moneyEarnedByPlayer;
+                float dealerMoney = dailySummary.moneyEarnedByDealers;
+                float total = playerMoney + dealerMoney;
+                //round total to 0 decimal places
+                total = (float)Math.Round(total, 0);
+                dealerMoney = (float)Math.Round(dealerMoney, 0);
+
+
+                dailySummary.DealerEarningsLabel.text = $"${dealerMoney} (${total})";
+
+            }
+
+        }
+
+
+
+
+    }
+}
