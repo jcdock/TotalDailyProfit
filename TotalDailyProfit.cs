f5ba9e60530600a9ced6d84e32a28c5bf6b5c5ce
Jc Dock <jcdock03@gmail.com>
2025-05-16 17:00:14 +1200

    change from using strings to Keycodes through Mod Manager

--- TotalDailyProfit.cs
+++ TotalDailyProfit.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 using Il2CppScheduleOne.UI;
 using Il2CppScheduleOne.Persistence.Datas;
 using ModManagerPhoneApp;
+using FishNet.Managing.Statistic;
 
 
 
@@ -22,37 +23,16 @@ namespace TotalDailyProfit
         {
            TotalDailyProfit.KeybindCategory = MelonPreferences.CreateCategory("TotalDailyProfit_Main", "Total Daily Profit Keybind Settings");
 
-           TotalDailyProfit.Keybind = TotalDailyProfit.KeybindCategory.CreateEntry<string>("Keybind", "F9", "Keybind to open/close the Daily Summary window.",null,false,false,null,null);
+           TotalDailyProfit.Keybind = TotalDailyProfit.KeybindCategory.CreateEntry<KeyCode>("Keybind", KeyCode.F9, "Keybind to open/close the Daily Summary window.",null,false,false,null,null);
            TotalDailyProfit.CrtlModifier = TotalDailyProfit.KeybindCategory.CreateEntry<bool>("CrtlModifier", false, "Use Ctrl + Keybind.", null, false, false, null, null);
            TotalDailyProfit.ShiftModifier = TotalDailyProfit.KeybindCategory.CreateEntry<bool>("ShiftModifier", false, "Use Shift + Keybind.", null, false, false, null, null);
            TotalDailyProfit.AltModifier = TotalDailyProfit.KeybindCategory.CreateEntry<bool>("AltModifier", false, "Use Alt + Keybind.", null, false, false, null, null);
-       
 
+           SubscribeToModManagerEvents();
 
-
-            SubscribeToModManagerEvents();
-
-       
-
-            harmony = new HarmonyLib.Harmony("com.jcdock.totaldailyprofit");
+           harmony = new HarmonyLib.Harmony("com.jcdock.totaldailyprofit");
            harmony.PatchAll();
            LoggerInstance.Msg(base.Info.Name +  " v" + base.Info.Version + " Initialized");
-        }
-
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-        {
-            if (sceneName == "Main")
-
-            {
-                var _keybind = TotalDailyProfit.Keybind.Value;
-             var isKeyValid = IsKeyBindValid(_keybind);
-                if (isKeyValid == false)
-                {
-                    TotalDailyProfit.Keybind.Value = "F9";
-                    LoggerInstance.Error("Keybind not set or is set to invalid key. Defaulting to F9.");
-                }
-               
-            }
         }
 
 
@@ -70,14 +50,14 @@ namespace TotalDailyProfit
 
             if (crtlModifier && shiftModifier && altModifier)
             {
-                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
                 }
             }
             else if (crtlModifier && shiftModifier)
             {
-                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
 
@@ -85,7 +65,7 @@ namespace TotalDailyProfit
             }
             else if (crtlModifier && altModifier)
             {
-                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
 
@@ -93,7 +73,7 @@ namespace TotalDailyProfit
             }
             else if (shiftModifier && altModifier)
             {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
 
@@ -101,28 +81,28 @@ namespace TotalDailyProfit
             }
             else if (crtlModifier)
             {
-                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
                 }
             }
             else if (shiftModifier)
             {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
                 }
             }
             else if (altModifier)
             {
-                if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
                 }
             }
             else
             {
-                if (Input.GetKeyDown(ParseKeybind(TotalDailyProfit.Keybind.Value)))
+                if (Input.GetKeyDown(TotalDailyProfit.Keybind.Value))
                 {
                     ToggleDailySummary();
                 }
@@ -165,39 +145,21 @@ namespace TotalDailyProfit
                 float playerMoney = dailySummary.moneyEarnedByPlayer;
                 float dealerMoney = dailySummary.moneyEarnedByDealers;
                 float totalMoney = playerMoney + dealerMoney;
-                //round total to 0 decimal places
+                //round total and dealierMoney to 0 decimal places
                 totalMoney = (float)Math.Round(totalMoney, 0);
                 dealerMoney = (float)Math.Round(dealerMoney, 0);
                 //format the numbers with commas
                var totalMoneyString = FormatNumber(totalMoney);
                 //format the dealer money with commas
                 var dealerMoneyString = FormatNumber(dealerMoney);
-
+              
 
                 dailySummary.DealerEarningsLabel.text = $"${dealerMoneyString} (${totalMoneyString})";
 
             }
         }
 
-        private bool IsKeyBindValid(string keybind)
-        {
-            bool flag = Enum.TryParse<KeyCode>(keybind, out KeyCode keyCode);
-            return flag;
-        }
-        private KeyCode ParseKeybind(string keybind)
-        {
-            bool flag = Enum.TryParse<KeyCode>(keybind, out KeyCode keyCode);
-            KeyCode result;
-            if (flag)
-            {
-                result = keyCode;
-            }
-            else
-            {
-                result = KeyCode.None;
-            }
-            return result;
-        }
+     
 
 
 
@@ -238,18 +200,12 @@ namespace TotalDailyProfit
         
             try
             {
-                TotalDailyProfit.Keybind = TotalDailyProfit.KeybindCategory.GetEntry<string>("Keybind");
+                TotalDailyProfit.Keybind = TotalDailyProfit.KeybindCategory.GetEntry<KeyCode>("Keybind");
                 TotalDailyProfit.CrtlModifier = TotalDailyProfit.KeybindCategory.GetEntry<bool>("CrtlModifier");
                 TotalDailyProfit.ShiftModifier = TotalDailyProfit.KeybindCategory.GetEntry<bool>("ShiftModifier");
                 TotalDailyProfit.AltModifier = TotalDailyProfit.KeybindCategory.GetEntry<bool>("AltModifier");
 
-                var _keybind = TotalDailyProfit.Keybind.Value;
-                var isKeyValid = IsKeyBindValid(_keybind);
-                if (isKeyValid == false)
-                {
-                    TotalDailyProfit.Keybind.Value = "F9";
-                    LoggerInstance.Error("Keybind not set or is set to invalid key. Defaulting to F9.");
-                }
+          
            
                 LoggerInstance.Msg("Settings reloaded successfully.");
             }
@@ -257,8 +213,7 @@ namespace TotalDailyProfit
         }
 
         private static MelonPreferences_Category KeybindCategory;
-        private static MelonPreferences_Entry<string> Keybind;
-        private static MelonPreferences_Entry<bool> debugMode;
+        private static MelonPreferences_Entry<KeyCode> Keybind;
         private static MelonPreferences_Entry<bool> CrtlModifier;
         private static MelonPreferences_Entry<bool> ShiftModifier;
         private static MelonPreferences_Entry<bool> AltModifier;
